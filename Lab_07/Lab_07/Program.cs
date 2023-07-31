db4a1e3a4b3ad8a3bc70e458bc00638b11d65518
loshini2002 <141049325+loshini2002@users.noreply.github.com>
2023-07-31 21:38:29 +0530

    Add files via upload

---
+++ Lab_07/Lab_07/Program.cs
@@ -0,0 +1,25 @@
+﻿using Lab_07.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ënter km value:");
+            double km = double.Parse(Console.ReadLine());
+
+            ConvertValues obj = new ConvertValues();
+            double m = obj.kmTOm(km);
+            Console.WriteLine(m.ToString());
+            //obj.kmTOm(km);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab_07/Lab_07/Properties/ConvertValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_07.Properties
+{
+    internal class ConvertValues
+    {
+        public double kmTOm(double km)
+        {
+            double m = km * 1000;
+            Console.WriteLine(m.ToString() + "m");
+            return m;
+        }
+    }
+}
